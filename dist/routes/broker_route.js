411fd160d9ff4b35bbf8dee76af3bca178f32481
nazeem <nazeemnob18@gmail.com>
2022-01-12 21:33:40 +0530

    oops

--- dist/routes/broker_route.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const broker_controller_1 = require("../controllers/broker_controller");
-const express_1 = require("express");
-const router = (0, express_1.Router)();
-router.post("/", broker_controller_1.brokerController);
-exports.default = router;
